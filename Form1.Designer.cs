261c67f80f1cd9294d07c63650931337119af951
Christopher Müller <chris@mechatronikmonkey.de>
2024-09-23 03:23:09 +0200

    Up and Download possible now
    Up and download possible.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.label10 = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.txtSettingsFile = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.txtPathToSave = new System.Windows.Forms.TextBox();
@@ -60,6 +60,10 @@
             this.txtFTP = new System.Windows.Forms.TextBox();
             this.txtStatus = new System.Windows.Forms.TextBox();
             this.checkFile = new System.Windows.Forms.Timer(this.components);
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabSync.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -108,6 +112,7 @@
             this.btnDOWNLOAD.Size = new System.Drawing.Size(120, 120);
             this.btnDOWNLOAD.TabIndex = 14;
             this.btnDOWNLOAD.UseVisualStyleBackColor = true;
+            this.btnDOWNLOAD.Click += new System.EventHandler(this.btnDOWNLOAD_Click);
             // 
             // btnUPLOAD
             // 
@@ -210,11 +215,15 @@
             // 
             // tabSettings
             // 
+            this.tabSettings.Controls.Add(this.label14);
+            this.tabSettings.Controls.Add(this.label13);
+            this.tabSettings.Controls.Add(this.label12);
+            this.tabSettings.Controls.Add(this.label11);
             this.tabSettings.Controls.Add(this.button4);
             this.tabSettings.Controls.Add(this.label10);
             this.tabSettings.Controls.Add(this.txtName);
             this.tabSettings.Controls.Add(this.label9);
-            this.tabSettings.Controls.Add(this.txtFilename);
+            this.tabSettings.Controls.Add(this.txtSettingsFile);
             this.tabSettings.Controls.Add(this.btnSave);
             this.tabSettings.Controls.Add(this.label8);
             this.tabSettings.Controls.Add(this.txtPathToSave);
@@ -234,7 +243,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(361, 205);
+            this.button4.Location = new System.Drawing.Point(360, 188);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(36, 23);
             this.button4.TabIndex = 13;
@@ -263,20 +272,20 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(24, 149);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(23, 13);
+            this.label9.Size = new System.Drawing.Size(61, 13);
             this.label9.TabIndex = 10;
-            this.label9.Text = "File";
+            this.label9.Text = "Settings file";
             // 
-            // txtFilename
+            // txtSettingsFile
             // 
-            this.txtFilename.Location = new System.Drawing.Point(89, 146);
-            this.txtFilename.Name = "txtFilename";
-            this.txtFilename.Size = new System.Drawing.Size(162, 20);
-            this.txtFilename.TabIndex = 9;
+            this.txtSettingsFile.Location = new System.Drawing.Point(89, 146);
+            this.txtSettingsFile.Name = "txtSettingsFile";
+            this.txtSettingsFile.Size = new System.Drawing.Size(162, 20);
+            this.txtSettingsFile.TabIndex = 9;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(40, 246);
+            this.btnSave.Location = new System.Drawing.Point(40, 249);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(331, 23);
             this.btnSave.TabIndex = 8;
@@ -287,17 +296,18 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(21, 209);
+            this.label8.Location = new System.Drawing.Point(20, 192);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.Size = new System.Drawing.Size(58, 13);
             this.label8.TabIndex = 7;
-            this.label8.Text = "path to save game";
+            this.label8.Text = "Savegame";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // txtPathToSave
             // 
-            this.txtPathToSave.Location = new System.Drawing.Point(122, 206);
+            this.txtPathToSave.Location = new System.Drawing.Point(89, 189);
             this.txtPathToSave.Name = "txtPathToSave";
-            this.txtPathToSave.Size = new System.Drawing.Size(233, 20);
+            this.txtPathToSave.Size = new System.Drawing.Size(265, 20);
             this.txtPathToSave.TabIndex = 6;
             // 
             // label7
@@ -359,8 +369,47 @@
             // checkFile
             // 
             this.checkFile.Enabled = true;
-            this.checkFile.Interval = 2000;
+            this.checkFile.Interval = 3000;
             this.checkFile.Tick += new System.EventHandler(this.checkFile_Tick);
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(273, 22);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(114, 31);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "DONT change after uploading under that name!";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(273, 72);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(95, 12);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "ftp://yourftpadress.de";
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(273, 146);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(126, 34);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "Your private settings file.  \r\nE.g. \"NameA_NameB.xml\"";
+            // 
+            // label14
+            // 
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(97, 212);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(288, 23);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "Leave empty if joining a game, pick file if starting an upload.\r\nWill be filled a" +
+    "utomatically by downloading a file.";
             // 
             // SatisfactorySync
             // 
@@ -412,11 +461,15 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.TextBox txtSettingsFile;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox txtStatus;
         private System.Windows.Forms.Timer checkFile;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label14;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -56,7 +56,7 @@ namespace V1
             sSSettings.ftpAddress = txtFTP.Text;
             sSSettings.user = txtUser.Text;
             sSSettings.pass = txtPass.Text;
-            sSSettings.file = txtFilename.Text;
+            sSSettings.file = txtSettingsFile.Text;
             sSSettings.pathToSave = txtPathToSave.Text;
 
             // Dateipfad zum Speichern der Settings festlegen
@@ -83,20 +83,43 @@ namespace V1
             txtFTP.Text = settings.ftpAddress;
             txtUser.Text = settings.user;
             txtPass.Text = settings.pass;
-            txtFilename.Text = settings.file;
+            txtSettingsFile.Text = settings.file;
             txtPathToSave.Text = settings.pathToSave;
 
         }
 
         private void btnUPLOAD_Click(object sender, EventArgs e)
         {
+
+            // 0. Vergleich der Datumswerte
+            DateTime lastUpdateDate = DateTime.Parse(txtLastUPdate.Text);
+            DateTime lastDownloadDate = DateTime.Parse(txtLastDOWNdate.Text);
+
+            if (txtName.Text != txtLastDOWNname.Text)
+            {
+                // Warnung, anderer User im Spiel
+                DialogResult result = MessageBox.Show(
+                    "Jemand anderes hat das Spiel nach dir heruntergeladen! Der User " + txtLastDOWNname.Text + " hat das spiel parallel zu dir heruntergeladen. Bist du sicher, dass du das Spiel trotzdem hochladen möchtest?",
+                    "Warnung",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.No)
+                {
+                    txtStatus.Text = "Uplad abgebrochen.";
+                    return;
+                }
+            }
+
             string xmlFilePath = Path.Combine(Path.GetTempPath(), "updownstate.xml");
+            string binaryFilePath = txtPathToSave.Text; // Absoluter Pfad zur bestehenden Binärdatei
             UP_DOWN_State settings;
 
-            // Prüfen, ob die Datei existiert
+            // Prüfen, ob die XML-Datei existiert
             if (File.Exists(xmlFilePath))
             {
-                // Datei lesen und bestehende Einstellungen laden
+                // XML-Datei lesen und bestehende Einstellungen laden
                 XmlSerializer serializer = new XmlSerializer(typeof(UP_DOWN_State));
                 using (FileStream fs = new FileStream(xmlFilePath, FileMode.Open))
                 {
@@ -105,15 +128,15 @@ namespace V1
             }
             else
             {
-                // Neue Instanz erstellen, wenn die Datei nicht existiert
+                // Neue Instanz erstellen, wenn die XML-Datei nicht existiert
                 settings = new UP_DOWN_State();
             }
 
             // Update der gewünschten Properties
-            settings.lastUPName = txtName.Text; // Zum Beispiel Property1 von textBox1
-            settings.lastUPDate = DateTime.Now.ToString(); // Zum Beispiel Property2 von textBox2
+            settings.lastUPName = txtName.Text;
+            settings.lastUPDate = DateTime.Now.ToString();
 
-            // XML-Datei speichern (entweder neu erstellen oder aktualisieren)
+            // XML-Datei speichern
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(UP_DOWN_State));
             using (FileStream fs = new FileStream(xmlFilePath, FileMode.Create))
             {
@@ -121,49 +144,85 @@ namespace V1
             }
 
             // FTP-Verbindungsinformationen
-            string ftpServer = txtFTP.Text; // Ersetze mit deinem FTP-Server
-            string ftpUser = txtUser.Text; // Ersetze mit deinem Benutzernamen
-            string ftpPass = txtPass.Text; // Ersetze mit deinem Passwort
+            string ftpServer = txtFTP.Text;
+            string ftpUser = txtUser.Text;
+            string ftpPass = txtPass.Text;
 
-            // Hochladen der XML-Datei
-            string ftpFullPath = $"{ftpServer}/updownstate.xml"; // Pfad auf dem FTP-Server
-
+            // 1. Binärdatei vom Dateisystem auf FTP hochladen (ohne sie zu verändern)
             try
             {
+                if (!File.Exists(binaryFilePath))
+                {
+                    MessageBox.Show("Das Savegame existiert nicht!");
+                    return;
+                }
+
+                // Extrahiere den Dateinamen (mit Erweiterung) aus dem lokalen Pfad
+                string binaryFileName = Path.GetFileName(binaryFilePath);
+
+                // Binärdatei auf FTP hochladen
+                string ftpBinaryPath = $"{ftpServer}/{binaryFileName}"; // Nutze den Dateinamen vom lokalen System
+                FtpWebRequest binaryRequest = (FtpWebRequest)WebRequest.Create(ftpBinaryPath);
+                binaryRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                binaryRequest.Credentials = new NetworkCredential(ftpUser, ftpPass);
+                binaryRequest.UsePassive = true;
+
+                byte[] binaryFileContents = File.ReadAllBytes(binaryFilePath); // Originale Binärdatei lesen
+                binaryRequest.ContentLength = binaryFileContents.Length;
+
+                using (Stream binaryRequestStream = binaryRequest.GetRequestStream())
+                {
+                    binaryRequestStream.Write(binaryFileContents, 0, binaryFileContents.Length);
+                }
+
+                using (FtpWebResponse binaryResponse = (FtpWebResponse)binaryRequest.GetResponse())
+                {
+                    MessageBox.Show($"Savegame erfolgreich hochgeladen: {binaryResponse.StatusDescription}");
+                }
+
+                txtStatus.Text = "Savegame erfolgreich hochgeladen.";
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = "Fehler beim Hochladen des Savegames: " + ex.Message;
+                return; // Abbrechen, falls der Upload fehlschlägt
+            }
+
+            // 2. XML-Datei hochladen
+            try
+            {
+                string ftpFullPath = $"{ftpServer}/updownstate.xml"; // Pfad auf dem FTP-Server
                 FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpFullPath);
                 request.Method = WebRequestMethods.Ftp.UploadFile;
-
                 request.Credentials = new NetworkCredential(ftpUser, ftpPass);
-                request.ContentLength = new FileInfo(xmlFilePath).Length;
+                request.UsePassive = true;
 
-                // Datei in einen Byte-Array lesen
                 byte[] fileContents;
                 using (StreamReader sourceStream = new StreamReader(xmlFilePath))
                 {
                     fileContents = System.Text.Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
                 }
 
-                // Datei hochladen
+                request.ContentLength = fileContents.Length;
+
                 using (Stream requestStream = request.GetRequestStream())
                 {
                     requestStream.Write(fileContents, 0, fileContents.Length);
                 }
 
-                // Antwort vom Server abrufen
                 using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
                 {
-                    MessageBox.Show($"Upload erfolgreich: {response.StatusDescription}");
+                    MessageBox.Show($"XML-Datei erfolgreich hochgeladen: {response.StatusDescription}");
                 }
 
                 txtStatus.Text = "state update pushed.";
             }
             catch (Exception ex)
             {
-                txtStatus.Text = "Fehler beim status update!" + ex.Message;
+                txtStatus.Text = "Fehler beim Hochladen der XML-Datei: " + ex.Message;
             }
-
-            
         }
+
 
         private void checkFile_Tick(object sender, EventArgs e)
         {
@@ -172,7 +231,7 @@ namespace V1
             string ftpUser = txtUser.Text; // Ersetze mit deinem Benutzernamen
             string ftpPass = txtPass.Text; // Ersetze mit deinem Passwort
 
-            string ftpFullPath = $"{ftpServer}/updownstate.xml"; // Pfad auf dem FTP-Server
+            string ftpFullPath = $"{ftpServer}/" + txtSettingsFile.Text; // Pfad auf dem FTP-Server
 
             try
             {
@@ -195,9 +254,9 @@ namespace V1
                         // TextBoxen mit den geladenen Werten füllen
                         txtLastUPname.Text = settings.lastUPName;
                         txtLastUPdate.Text = settings.lastUPDate;
-                        // Hier kannst du die anderen Properties zuweisen, falls nötig
-                        // textBox3.Text = settings.Property3;
-                        // textBox4.Text = settings.Property4;
+                        txtLastDOWNname.Text = settings.lastDOWNName;
+                        txtLastDOWNdate.Text = settings.lastDOWNDate;
+
                     }
                 }
 
@@ -207,6 +266,218 @@ namespace V1
             {
                 txtStatus.Text = "Fehler FTP!" + ex.Message;
             }
+        }
+
+        // Methode zur Dateiauswahl (z.B. durch eine ComboBox oder ListBox)
+        private string PromptForFileSelection(List<string> files)
+        {
+            using (Form form = new Form())
+            {
+                ListBox listBox = new ListBox { Dock = DockStyle.Fill };
+                foreach (var file in files)
+                {
+                    listBox.Items.Add(file);
+                }
+
+                form.Controls.Add(listBox);
+                form.StartPosition = FormStartPosition.CenterParent;
+                form.Size = new Size(300, 200);
+
+                System.Windows.Forms.Button selectButton = new System.Windows.Forms.Button { Text = "Auswählen", Dock = DockStyle.Bottom };
+                form.Controls.Add(selectButton);
+
+                string selectedFile = null;
+                selectButton.Click += (sender, e) =>
+                {
+                    selectedFile = listBox.SelectedItem?.ToString();
+                    form.DialogResult = DialogResult.OK;
+                    form.Close();
+                };
+
+                form.ShowDialog();
+                return selectedFile;
+            }
+        }
+
+        private void btnDOWNLOAD_Click(object sender, EventArgs e)
+        {
+
+            // 0. Vergleich der Datumswerte
+            DateTime lastUpdateDate = DateTime.Parse(txtLastUPdate.Text);
+            DateTime lastDownloadDate = DateTime.Parse(txtLastDOWNdate.Text);
+
+            if (lastDownloadDate > lastUpdateDate)
+            {
+                // Warnung, wenn das letzte Download-Datum jünger ist als das Upload-Datum
+                DialogResult result = MessageBox.Show(
+                    "Das letzte Download-Datum ist jünger als das Upload-Datum. Das könnte bedeuten, dass die Datei aktuell von Benutzer "+txtLastDOWNname.Text+" genutzt wird. Möchten Sie trotzdem fortfahren?",
+                    "Warnung",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.No)
+                {
+                    txtStatus.Text = "Download abgebrochen.";
+                    return;
+                }
+            }
+
+            // 1. Liste der .sav-Dateien auf dem FTP-Server abrufen
+            string ftpServer = txtFTP.Text;
+            string ftpUser = txtUser.Text;
+            string ftpPass = txtPass.Text;
+            List<string> savFiles = new List<string>();
+
+            try
+            {
+                FtpWebRequest listRequest = (FtpWebRequest)WebRequest.Create(ftpServer);
+                listRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                listRequest.Credentials = new NetworkCredential(ftpUser, ftpPass);
+
+                using (FtpWebResponse listResponse = (FtpWebResponse)listRequest.GetResponse())
+                using (StreamReader reader = new StreamReader(listResponse.GetResponseStream()))
+                {
+                    string line = null;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.EndsWith(".sav"))
+                        {
+                            savFiles.Add(line); // Alle .sav-Dateien sammeln
+                        }
+                    }
+                }
+
+                if (savFiles.Count == 0)
+                {
+                    MessageBox.Show("Keine .sav-Dateien auf dem Server gefunden.");
+                    return;
+                }
+
+                // 2. Benutzer kann eine Datei auswählen
+                string selectedFile = PromptForFileSelection(savFiles); // Methode zum Auswählen der Datei
+                if (string.IsNullOrEmpty(selectedFile))
+                {
+                    MessageBox.Show("Keine Datei ausgewählt.");
+                    return;
+                }
+
+                // 3. Zielpfad angeben lassen
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    FileName = selectedFile,
+                    Filter = "SAV files (*.sav)|*.sav|All files (*.*)|*.*"
+                };
+
+                if (saveFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    MessageBox.Show("Kein Zielpfad angegeben.");
+                    return;
+                }
+
+                string localFilePath = saveFileDialog.FileName;
+
+                // 4. Datei vom FTP-Server herunterladen
+                try
+                {
+                    string ftpFilePath = $"{ftpServer}/{selectedFile}";
+                    FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(ftpFilePath);
+                    downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                    downloadRequest.Credentials = new NetworkCredential(ftpUser, ftpPass);
+
+                    using (FtpWebResponse downloadResponse = (FtpWebResponse)downloadRequest.GetResponse())
+                    using (Stream responseStream = downloadResponse.GetResponseStream())
+                    using (FileStream fileStream = new FileStream(localFilePath, FileMode.Create))
+                    {
+                        responseStream.CopyTo(fileStream);
+                    }
+
+                    MessageBox.Show("Savegame erfolgreich heruntergeladen.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Fehler beim Herunterladen des Savegames: " + ex.Message);
+                    return;
+                }
+
+                // 5. XML-Datei mit neuen Properties aktualisieren
+                string xmlFilePath = Path.Combine(Path.GetTempPath(), "updownstate.xml");
+                UP_DOWN_State settings;
+
+                if (File.Exists(xmlFilePath))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(UP_DOWN_State));
+                    using (FileStream fs = new FileStream(xmlFilePath, FileMode.Open))
+                    {
+                        settings = (UP_DOWN_State)serializer.Deserialize(fs);
+                    }
+                }
+                else
+                {
+                    settings = new UP_DOWN_State();
+                }
+
+                // Properties aktualisieren (wie vorher beim Upload)
+                settings.lastDOWNName = txtName.Text;
+                settings.lastDOWNDate = DateTime.Now.ToString();
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(UP_DOWN_State));
+                using (FileStream fs = new FileStream(xmlFilePath, FileMode.Create))
+                {
+                    xmlSerializer.Serialize(fs, settings);
+                }
+
+                // 6. Aktualisierte XML-Datei auf den FTP-Server hochladen
+                try
+                {
+                    string ftpXmlPath = $"{ftpServer}/updownstate.xml"; // Pfad auf dem FTP-Server
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(ftpXmlPath);
+                    request.Method = WebRequestMethods.Ftp.UploadFile;
+                    request.Credentials = new NetworkCredential(ftpUser, ftpPass);
+                    request.UsePassive = true;
+
+                    // Datei in einen Byte-Array lesen
+                    byte[] fileContents;
+                    using (StreamReader sourceStream = new StreamReader(xmlFilePath))
+                    {
+                        fileContents = System.Text.Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
+                    }
+
+                    request.ContentLength = fileContents.Length;
+
+                    // Datei hochladen
+                    using (Stream requestStream = request.GetRequestStream())
+                    {
+                        requestStream.Write(fileContents, 0, fileContents.Length);
+                    }
+
+                    // Antwort vom Server abrufen
+                    using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                    {
+                        MessageBox.Show($"XML-Datei erfolgreich hochgeladen: {response.StatusDescription}");
+                    }
+
+                    txtStatus.Text = "state update pushed.";
+                }
+                catch (Exception ex)
+                {
+                    txtStatus.Text = "Fehler beim Hochladen der XML-Datei: " + ex.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                txtStatus.Text = "Fehler: " + ex.Message;
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
         }
     }
 
