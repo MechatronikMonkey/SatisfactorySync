a8a3991b8c57156fbc4e71132b4cf73b32baa337
Christopher Müller <chris@mechatronikmonkey.de>
2024-10-22 12:25:56 +0200

    Added DataGrid for logs

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -17,11 +17,27 @@ namespace SatisfatorySync.ViewModels
         public string RemotePlaytime { get; set; } = "12h 45m";
         public string RemoteLastPushName { get; set; } = "Test Remote User";
         public string RemoteLastPushDate { get; set; } = "2024-10-21";
-
+        public ObservableCollection<LogEntry> LogEntries { get; set; }
 
         public MainWindowViewModel() // Constructor
         {
-            
+            LogEntries = new ObservableCollection<LogEntry>
+            {
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" },
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" },
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" },
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" },
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" },
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" },
+                new LogEntry { TimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), Action = "Initial Action", Result = "Completed" }
+            };
+        }
+
+        public class LogEntry
+        {
+            public string TimeStamp { get; set; }
+            public string Action { get; set; }
+            public string Result { get; set; }
         }
 
 #pragma warning restore CA1822 // Mark members as static
